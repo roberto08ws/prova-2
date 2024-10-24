9e276e29f8ee22f18556679a9e12d0acaebfcfb0
roberto08ws <roberto.henrique.08.ws@gmail.com>
2024-10-24 10:30:02 -0300

    Setando o layout no recyclerView

--- app/src/main/java/com/example/prova4/TelaRanking.java
+++ app/src/main/java/com/example/prova4/TelaRanking.java
@@ -1,5 +1,6 @@
 package com.example.prova4;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,18 +8,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.prova4.databinding.ActivityTelaRankingBinding;
 
 public class TelaRanking extends AppCompatActivity {
+
+    private ActivityTelaRankingBinding view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_tela_ranking);
+        view = ActivityTelaRankingBinding.inflate(getLayoutInflater());
+        setContentView(view.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        view.recyclerView.setLayoutManager(new LinearLayoutManager(TelaRanking.this));
+        view.recyclerView.setHasFixedSize(true);
+
     }
 }
