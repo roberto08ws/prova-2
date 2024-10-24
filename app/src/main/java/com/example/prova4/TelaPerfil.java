4c2d4eddb5c565b19d939f82fe8dac539ba27099
roberto08ws <roberto.henrique.08.ws@gmail.com>
2024-10-24 10:03:41 -0300

    Informações do usuário passadas para os campos

--- app/src/main/java/com/example/prova4/TelaPerfil.java
+++ app/src/main/java/com/example/prova4/TelaPerfil.java
@@ -1,5 +1,7 @@
 package com.example.prova4;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +10,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.prova4.databinding.ActivityTelaPerfilBinding;
+
 public class TelaPerfil extends AppCompatActivity {
+
+    private ActivityTelaPerfilBinding view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_tela_perfil);
+        view = ActivityTelaPerfilBinding.inflate(getLayoutInflater());
+        setContentView(view.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        SharedPreferences cache = getSharedPreferences("cadastro", MODE_PRIVATE);
+
+        String nome = cache.getString("nome", null);
+        String cidade = cache.getString("cidade", null);
+        String curso = cache.getString("curso", null);
+
+        view.edtNome.setText(nome);
+        view.edtCidade.setText(cidade);
+        view.edtCurso.setText(curso);
+
+        view.edtCurso.setEnabled(false);
+
     }
 }
