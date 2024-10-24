a9a18cbb4e7da8d782a3b6a288cab33574eece30
roberto08ws <roberto.henrique.08.ws@gmail.com>
2024-10-24 09:26:09 -0300

    Listando todos os cursos para o comboBox

--- app/src/main/java/com/example/prova4/MainActivity.java
+++ app/src/main/java/com/example/prova4/MainActivity.java
@@ -11,9 +11,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.prova4.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding view;
     private String[] cursos;
+    private List<Cursos> list = new ArrayList<>();
 
     private interface Lista {
 
@@ -44,7 +48,24 @@ public class MainActivity extends AppCompatActivity {
 
         Retrofit retrofit = new Retrofit.Builder().baseUrl("http://10.0.2.2:3000").addConverterFactory(GsonConverterFactory.create()).build();
 
+        Lista lista = retrofit.create(Lista.class);
 
+        lista.lista().enqueue(new Callback<List<Cursos>>() {
+            @Override
+            public void onResponse(Call<List<Cursos>> call, Response<List<Cursos>> response) {
+
+                list = response.body();
+                cursos = new String[list.size()];
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Cursos>> call, Throwable throwable) {
+
+
+
+            }
+        });
 
     }
 }
